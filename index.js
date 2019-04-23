1314285557c708503519d8301462a32e850fcd01
DangerN <cdneal091@gmail.com>
2019-04-23 11:28:01 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file
+function findMatching(drivers, name) {
+  return drivers.filter(function (potoat) { return potoat.toLowerCase() === name.toLowerCase()})
+}
+
+function fuzzyMatch(drivers, name) {
+  return drivers.filter(function (yeet){ return yeet.charAt(0) === name.charAt(0)})
+}
+
+function matchName(drivers, name) {
+  console.log(drivers)
+  console.log(name)
+  return (drivers.filter(function (driver) { return driver.name === name}))
+}
